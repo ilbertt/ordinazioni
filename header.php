18b57d2ccec249771666de6c6fdef388be16927b
Luca Bertelli <luca.berte98@hotmail.it>
2019-05-21 22:52:42 +0200

    semantic libs imported correctly

--- header.php
+++ header.php
@@ -10,18 +10,13 @@ require_once 'config.php';
 <html>
     <head>
         <title>Ordina online</title>
+        <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
         <link href="default.css" rel='stylesheet' type='text/css'/>
         <script src='jquery-3.4.1.min.js'></script> <!--TODO: INCLUDE jquery library-->
         <script src='funcs.js'></script>
-        <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
-        <script src="semantic/dist/semantic.min.js"></script>
+
+        <script src="semantic/semantic.min.js"></script>
     </head>
     <body>
         <div class="content">
-            <!-- <p class="title"><span>F</span>esta <span>M</span>aria <span>A</span>usiliatrice 20<span>19</span></p> -->
-        <div class="ui grid">
-            <div class="four wide column"></div>
-            <div class="four wide column"></div>
-            <div class="four wide column"></div>
-            <div class="four wide column"></div>
-        </div>
+            <p class="title"><span>F</span>esta <span>M</span>aria <span>A</span>usiliatrice 20<span>19</span></p>
